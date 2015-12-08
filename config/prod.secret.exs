9f41d2b91dbe09d355b0a3a563780718918728fb
Amanda Sposito <amandinha.sposito@gmail.com>
2015-12-08 19:13:01 -0200

    add: trying to deploy to heroku

---
+++ config/prod.secret.exs
@@ -0,0 +1,18 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+config :demo_chat, DemoChat.Endpoint,
+  http: [port: {:system, "PORT"}],
+  url: [host: "papo-reto-demo-chat.herokuapp.com", port: 80],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+# Configure your database
+# config :demo_chat, DemoChat.Repo,
+#   adapter: Ecto.Adapters.Postgres,
+#   username: "postgres",
+#   password: "postgres",
+#   database: "demo_chat_prod",
+#   pool_size: 20

6611dce2c5acb736bd10287ec61263c7dd954d8c
Amanda Sposito <amandinha.sposito@gmail.com>
2015-12-08 19:19:20 -0200

    fix: database config

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -9,7 +9,7 @@ config :demo_chat, DemoChat.Endpoint,
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
-Configure your database
+# Configure your database
 config :demo_chat, DemoChat.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: System.get_env("DATABASE_USERNAME"),
